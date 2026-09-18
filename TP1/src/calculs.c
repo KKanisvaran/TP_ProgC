af1bcc179555a8db282475ce31e1c478f2722736
Kanisvaran06 <kanisvaran06@gmail.com>
2026-09-18 09:13:11 +0000

    Exo 1.8 Done

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,60 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int num1 = 16;
+    int num2 = 3;
+    char op = '*';
+
+    switch (op) {
+
+        case '+':
+            printf("Addition : %d\n", num1 + num2);
+            break;
+
+        case '-':
+            printf("Soustraction : %d\n", num1 - num2);
+            break;
+
+        case '*':
+            printf("Multiplication : %d\n", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                printf("Division : %d\n", num1 / num2);
+            }
+            else {
+                printf("Division par zero impossible\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                printf("Modulo : %d\n", num1 % num2);
+            }
+            else {
+                printf("Modulo par zero impossible\n");
+            }
+            break;
+
+        case '&':
+            printf("ET binaire : %d\n", num1 & num2);
+            break;
+
+        case '|':
+            printf("OU binaire : %d\n", num1 | num2);
+            break;
+
+        case '~':
+            printf("NON binaire : %d\n", ~num1);
+            break;
+
+        default:
+            printf("Operateur inconnu\n");
+            break;
+    }
+
+    return 0;
+}
